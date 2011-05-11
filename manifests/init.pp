629e226a83fabd6a773dab30b62dc625dd2d985e
Pietro Monteiro <pietro@deckmonitoring.com>
2011-05-11 11:15:15 -0700

    Initial commit. Install and setup the cronjob to run the scout agent.

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Manages scoutapp.com agent
+class scout {
+  package {
+    'scout':
+      ensure   => 'installed',
+      provider => 'gem';
+  }
+
+  # set up the cronjob that runs the agent every minute
+  if $scout_key {
+    cron {
+      'scout':
+        command => "/var/lib/gems/1.8/bin/scout ${scout_key}";
+    }
+  }
+}
---
+++ manifests/mysql.pp
@@ -0,0 +1,4 @@
+class scout::mysql {
+  # Install ruby mysql library using mysql module.
+  include mysql::client::ruby
+}
