3c695f788a53c43623e192d643b6c908a942312f
Gilbert Roulot <gilbert.roulot@tech-angels.com>
2013-07-25 08:40:42 +0000

    Removes Systemtimer gem
    It's only usefull for ruby 1.8

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # Manages scoutapp.com agent
 class scout($scout_key=false) {
   package {
-    ['scout', 'SystemTimer', 'elif', 'request-log-analyzer']:
+    ['scout', 'elif', 'request-log-analyzer']:
       ensure   => 'installed',
       provider => 'gem';
   }
